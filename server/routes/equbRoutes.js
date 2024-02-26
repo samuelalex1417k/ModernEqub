a6b2372994123d3df86a89b43b1ea6aa13bf2c42
natnaely <natnaelky2020@gmail.com>
2024-02-26 22:48:55 +0000

    Implemented equb and members management

--- server/routes/equbRoutes.js
+++ server/routes/equbRoutes.js
@@ -7,13 +7,18 @@ const {
 } = require("../middlewares/validation");
 const {
   createEqub,
+  addUsersToEqub,
+  removeUsersFromEqub,
   getEqubs,
   getEqub,
   getMyEqub,
+  getJoinedEqubs,
   getNearbyEqubs,
   regenerateCode,
   updateEqub,
   authorizeEqub,
+  leaveEqub,
+  endEqub,
   deleteEqub,
 } = require("../controllers/equbController");
 
@@ -22,10 +27,18 @@ router
   .post(protect, equbCreationValidation, validate, createEqub)
   .get(protect, admin, getEqubs);
 router.get("/search", protect, getEqub);
-router.get("/my", protect, getMyEqub);
+router.get("/myequb", protect, getMyEqub);
+router.get("/joined", protect, getJoinedEqubs);
 router.get("/:latitude/:longitude/nearby", protect, getNearbyEqubs);
 router.put("/:id/regenerate-code", protect, regenerateCode);
 router.post("/:id/authorize", protect, admin, authorizeEqub);
-router.route("/:id").put(protect, updateEqub).delete(protect, deleteEqub);
+router.post("/:id/addmember", protect, addUsersToEqub);
+router.put("/:id/removemember", protect, removeUsersFromEqub);
+router.put("/:id/leave", protect, leaveEqub);
+router.put("/:id/end", protect, endEqub);
+router
+  .route("/:id")
+  .put(protect, updateEqub)
+  .delete(protect, admin, deleteEqub);
 
 module.exports = router;
